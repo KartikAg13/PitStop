8fe89daf6462106611ec9240932b693f9a7f25a3
KartikAg13 <23803013@mail.jiit.ac.in>
2025-06-13 16:58:02 +0530

    almost ready

--- src/data/fetch_quali.py
+++ src/data/fetch_quali.py
@@ -6,13 +6,38 @@ from src.db.engine import getEngine
 from src.db.tables import getQualifyingTable, TABLE_NAME
 
 def parseTimeDelta(time_delta) -> float | None:
-	if pd.isna(time_delta) or time_delta is None:
+	if time_delta is None or (isinstance(time_delta, float) and pd.isna(time_delta)):
 		return None
+	elif isinstance(time_delta, float):
+		return time_delta
 	try:
 		return time_delta.total_seconds()
-	except Exception as e:
-		print(f"Error in parseTimeDelta(): {e}")
-		return None
+	except:
+		try:
+			return float(time_delta)
+		except  Exception as e:
+			print(f"Error in parseTimeDelta: {e}")
+			return None
+		
+def getWeather(laps: pd.DataFrame, weather: pd.DataFrame, target_second: float, tolerance: float = 31.0):
+	if laps is None or laps.empty or weather is None or weather.empty:
+		return (None, ) * 7
+	
+	laps_copy = laps[laps['LapTime'].notna()].copy()		# remove the NaT valued rows, removes the In laps and Out laps, keeps Flying Laps and Slow Laps
+	laps_copy['LapTime_seconds'] = laps_copy['LapTime'].dt.total_seconds()		# convert time to seconds for comparison
+	row = (laps_copy['LapTime_seconds'] - target_second).abs().idxmin()		# get the row index of closest time to q1/q2/q3 time (laptime in laps dataframe with minimum difference to laptime in results dataframe)
+
+	time = laps_copy.loc[row, 'Time']
+	time = time.total_seconds() if isinstance(time, pd.Timedelta) else time		# convert the time located to seconds
+
+	weather_copy = weather.copy()
+	weather_copy['Time_seconds'] = weather_copy['Time_seconds'].dt.total_seconds()
+	weather_copy['Time_seconds'] = pd.to_numeric(weather_copy['Time_seconds'], errors='coerce')
+	time = pd.to_numeric(time, errors='coerce')
+	index = (weather_copy['Time_seconds'] - time).abs().idxmin()	# search for the time in the weather dataframe to get the weather
+	data = weather_copy.loc[index]
+
+	return data["AirTemp"], data["TrackTemp"], data["Humidity"], data["Pressure"], data["WindSpeed"], data["Rainfall"], data["WindDirection"]
 
 def fetchQualifyingData(start_year: int, end_year: int):
 	
@@ -24,44 +49,40 @@ def fetchQualifyingData(start_year: int, end_year: int):
 		
 		for weekend in range(1, 25):
 			try:
+				event = get_event(year, weekend)
+				event_name = event['EventName']
 				session = get_session(year, weekend, 'Q')
-				session.load()
-				dataframe = session.results
-				
-				with engine.begin() as connection:
-					for row in dataframe.itertuples(index=False):
-						driver_name = getattr(row, "Abbreviation")
-						q1_time = parseTimeDelta(getattr(row, "Q1"))
-						q2_time = parseTimeDelta(getattr(row, "Q2"))
-						q3_time = parseTimeDelta(getattr(row, "Q3"))
-						company = getattr(row, "TeamName")
+				session.load(weather=True)
+				results = session.results
+				laps = session.laps
+				weather = session.weather_data
 
-						insert_statement = qualifying.insert().values( 
-							season = year, round = weekend,
-							driver = driver_name,
-							q1 = q1_time, q2 = q2_time, q3 = q3_time,
-							team = company
-						)
-						connection.execute(insert_statement)
-					
-					print(f"Round {weekend} for year {year} successfully stored")
+				with engine.begin() as connection:
+					for row in results.itertuples():
+						number = int(getattr(row, "DriverNumber"))
+						name = str(getattr(row, "Abbreviation"))
+						company = str(getattr(row, "TeamName"))
+
+						q1_time: float | None = parseTimeDelta(getattr(row, "Q1", None))
+						q2_time: float | None = parseTimeDelta(getattr(row, "Q2", None))
+						q3_time: float | None = parseTimeDelta(getattr(row, "Q3", None))
 
 			except Exception as e:
 				print(f"Error in fetchQualifyingData(): {e}")
 
-# def trying():
-# 	Cache.enable_cache('cache/')
-# 	session = get_session(2023, 20, 'Q')
-# 	event = get_event(2023, 20)
-# 	session.load(weather=True)
-# 	lap = session.laps
-# 	data = session.results
-# 	weather = session.weather_data
-# 	print(data.info())
-# 	print("Weather Data:")
-# 	print(weather)
-# 	print("Lap Data:")
-# 	print(lap)
-# 	print("Lap Data Info:")
-# 	print(lap.info())
-# 	print(event['EventName'])
+def trying():
+	Cache.enable_cache('cache/')
+	session = get_session(2023, 20, 'Q')
+	event = get_event(2023, 20)
+	session.load(weather=True)
+	lap = session.laps
+	data = session.results
+	weather = session.weather_data
+	print(data.info())
+	print("Weather Data:")
+	print(weather)
+	print("Lap Data:")
+	print(lap)
+	print("Lap Data Info:")
+	print(lap.info())
+	print(event['EventName'])
