a005bfa8327c7ff23973e23ef5ac5b8bad00ee3e
KartikAg13 <23803013@mail.jiit.ac.in>
2025-06-22 06:41:27 +0530

    start training

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.model.q3_prediction import model
-
-def main() -> None:
-	model()
-
-if __name__ == "__main__":
-	main()
--- src/model/q3_prediction.py
+++ src/model/q3_prediction.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
 
 from src.data.preprocess import preprocessData
+from src.utils.print_utils import dataInfo
 
 class Q3Prediction:
 	
@@ -26,7 +27,7 @@ class Q3Prediction:
 		data['improvement'] = (data['q1'] - data['q2']) / data['q1']
 
 		# Check for possible rain
-		data['rain'] = (data['rain_flag_q1'] | data['rain_flag_q2']).astype(int)
+		data['rain'] = (data['rain_flag_q1'].astype(int) | data['rain_flag_q2'].astype(int))
 
 		# Get the change in weather
 		data['avg_air'] = (data['air_temp_q1'] + data['air_temp_q2']) / 2.0
@@ -39,17 +40,21 @@ class Q3Prediction:
 	def handleCategorical(self, features, data: pd.DataFrame) -> pd.DataFrame:
 
 		for column in features:
-
-			if column not in self.label_encoders:
-				self.label_encoders[column] = LabelEncoder()
-				data[column] = self.label_encoders[column].fit_transform(data[column].astype(str))
-			else:
-				data[column] = self.label_encoders[column].transform(data[column].astype(str))
+			
+			try:
+				if column not in self.label_encoders:
+					self.label_encoders[column] = LabelEncoder()
+					data[column] = self.label_encoders[column].fit_transform(data[column].astype(str))
+				else:
+					data[column] = self.label_encoders[column].transform(data[column].astype(str))
+			except:
+				# Handle exception for rookies of 2025
+				data[column] = -1
 
 		return data
 
 
-	def train_model(self, x_train: pd.DataFrame, y_train: pd.DataFrame):
+	def train_model(self, x_train, y_train):
 		
 		parameter_grid = {
 			"n_estimators": [100, 200, 300],
@@ -61,7 +66,7 @@ class Q3Prediction:
 
 		self.model = xgb.XGBRegressor(objective='reg:squarederror')
 
-		grid_search = GridSearchCV(self.model, parameter_grid, scoring='neg_mean_absolute_error', n_jobs=-1)
+		grid_search = GridSearchCV(self.model, parameter_grid, cv=10 ,scoring='neg_mean_absolute_error', n_jobs=-1, verbose=2)
 		grid_search.fit(x_train, y_train)
 		
 		self.model = grid_search.best_estimator_
@@ -98,6 +103,11 @@ class Q3Prediction:
 		train_data = data_copy[data_copy['season'] < 2025]
 		test_data = data_copy[data_copy['season'] == 2025]
 
+		# print("Training Data Info:")
+		# dataInfo(train_data)
+		# print("Testing Data Info:")
+		# dataInfo(test_data)
+
 		y_train = train_data['q3']
 		y_test = test_data['q3']
 
@@ -111,6 +121,9 @@ class Q3Prediction:
 		x_train = self.feature_engineering(x_train)
 		x_test = self.feature_engineering(x_test)
 
-		if isinstance(y_train, pd.DataFrame) and isinstance(y_test, pd.DataFrame):
-			self.train_model(x_train, y_train)
-			self.predict(x_test, y_test)
+		self.train_model(x_train, y_train)
+		self.predict(x_test, y_test)
+
+	
+	def load(self):
+		joblib.load("q3_prediction.joblib")
